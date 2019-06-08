f12280451e0f06fc8ec786f687cb8961fc295dc9
JOSE <jchipana@avances.com.pe>
2019-06-08 01:31:36 -0500

    controller security

---
+++ project/Areas/security/Controllers/SecurityController}.cs
@@ -0,0 +1,13 @@
+using System.Web.Mvc;
+
+namespace project.Areas.security.Controllers
+{
+    public class SecurityController_ : Controller
+    {
+        // GET
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
