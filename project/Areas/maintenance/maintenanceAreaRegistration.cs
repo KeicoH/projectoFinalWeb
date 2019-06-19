f032fddfd249fe0d1ad0ddce9d4600968bf22c34
JoseChipana <josepaulct@gmail.com>
2019-06-19 01:18:25 -0500

    -- new areas -- libraries icons & charts

---
+++ project/Areas/maintenance/maintenanceAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace project.Areas.maintenance
+{
+    public class maintenanceAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "maintenance";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "maintenance_default",
+                "maintenance/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ project/Areas/request/requestAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace project.Areas.request
+{
+    public class requestAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "request";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "request_default",
+                "request/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
